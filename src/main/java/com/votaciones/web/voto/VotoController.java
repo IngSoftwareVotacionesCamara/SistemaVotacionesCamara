af57a9a1eea3b8536a40c931ae88a27204584428
Stefany Rueda Garzón <s12r18g@gmail.com>
2025-10-29 01:00:45 -0500

    cargue VI

--- src/main/java/com/votaciones/web/voto/VotoController.java
+++ src/main/java/com/votaciones/web/voto/VotoController.java
@@ -1,4 +1,15 @@
 package com.votaciones.web.voto;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/votar")
 public class VotoController {
+
+    @GetMapping
+    public String votar() {
+        return "voto/votar-form";
+    }
 }
